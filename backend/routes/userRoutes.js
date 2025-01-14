50b19fa089d5dc58cbf59ebc0a047dbebe98a074
Gaurav K. Prasad <gaurav14112005@gmail.com>
2025-01-14 13:27:59 +0530

    Routing in wrong place while registering user on /register -> /

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,7 +10,9 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+// ! Post router missing on "/"
+router.post('/', registerUser);
+
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router
