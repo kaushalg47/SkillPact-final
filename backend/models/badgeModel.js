1407a605a0425e276a736deebd5d43b036d7caae
Gaurav Kapildeo Prasad <gaurav14112005@gmail.com>
2025-01-22 17:00:57 +0530

    returing badges, added more badges in schema, made a new route for getting user by id without requiring authentication

--- backend/models/badgeModel.js
+++ backend/models/badgeModel.js
@@ -4,7 +4,23 @@ const badgeSchema = new mongoose.Schema({
   title: { 
     type: String, 
     required: true, 
-    enum: ["Team Player", "Innovative", "Leadership", "Problem Solver", "Hardworking"], // Predefined badges
+    enum: [
+      "Team Player", 
+      "Innovative", 
+      "Leadership", 
+      "Problem Solver", 
+      "Hardworking",
+      "Creative Thinker",
+      "Dependable",
+      "Motivator",
+      "Efficient",
+      "Strategic Planner",
+      "Mentor",
+      "Collaborator",
+      "Visionary",
+      "Achiever",
+      "Communicator"
+    ], // Added more badges
   },
 });
 
