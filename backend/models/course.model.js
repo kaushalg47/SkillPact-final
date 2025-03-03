88d1fc95961c9a96d5f608005016b5fa3299c23f
Gaurav Kapildeo Prasad <gaurav14112005@gmail.com>
2025-03-03 16:47:46 +0530

    Updated the course model

--- backend/models/course.model.js
+++ backend/models/course.model.js
@@ -5,38 +5,52 @@ const courseSchema = new mongoose.Schema(
 		courseTitle: {
 			type: String,
 			required: true,
+			minlength: 2,
+			maxlength: 100,
 		},
-		subTitle: { type: String },
-		description: { type: String },
+		subTitle: { type: String, maxlength: 200 },
+		description: { type: String, maxlength: 3000 },
 		category: {
 			type: String,
 			required: true,
+			minlength: 2,
+			maxlength: 50,
 		},
 		courseLevel: {
 			type: String,
 			enum: ["Beginner", "Medium", "Advance"],
+			required: true,
 		},
 		coursePrice: {
 			type: Number,
+			min: 0,
+			default: 0,
 		},
 		courseThumbnail: {
 			type: String,
 		},
-		enrolledStudents: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "User",
-			},
-		],
-		lectures: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: "Lecture",
-			},
-		],
+		enrolledStudents: {
+			type: [
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "User",
+				},
+			],
+			default: [],
+		},
+		lectures: {
+			type: [
+				{
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Lecture",
+				},
+			],
+			default: [],
+		},
 		creator: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: "User",
+			required: true,
 		},
 		isPublished: {
 			type: Boolean,
@@ -45,7 +59,7 @@ const courseSchema = new mongoose.Schema(
 		badges: {
 			type: [
 				{
-					type: mongoose.Schema.ObjectId,
+					type: mongoose.Schema.Types.ObjectId,
 					ref: "Badge",
 				},
 			],
