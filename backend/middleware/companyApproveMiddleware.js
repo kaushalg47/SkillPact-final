e60ab88611ce06268ba9fdaadb0048680a1366ad
Gaurav Kapildeo Prasad <gaurav14112005@gmail.com>
2025-03-06 16:04:55 +0530

    using previously found company to verify if company is accepted

--- backend/middleware/companyApproveMiddleware.js
+++ backend/middleware/companyApproveMiddleware.js
@@ -1,29 +1,7 @@
 import asyncHandler from "express-async-handler";
-import Company from "../models/companyModel.js";
 
 const isCompanyAccepted = asyncHandler(async (req, res, next) => {
 	let company = res.locals.company;
-
-	// TODO: COMPANY_EXISTS MIDDLEWARE ALWAYS RUNS FIRST
-	// ! REMOVE THIS AFTER FIXING DEPENDENCIES
-	if (!company) {
-		if (req.params.compId) {
-			// If user is looking for company by get request
-			company = await Company.findById(req.params.compId);
-		} else {
-			// used if user is trying to post job -> used to see if the user has a company or not
-			company = await Company.findOne({ userId: req.user._id });
-		}
-
-		res.locals.company = company;
-	}
-
-	if (!company) {
-		return res.status(404).json({
-			message: "Company not found.",
-			success: false,
-		});
-	}
 
 	if (company.status) {
 		switch (company.status) {
