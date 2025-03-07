5eeb75552145c6f65fc26306186cfdaed60b897f
Gaurav Kapildeo Prasad <gaurav14112005@gmail.com>
2025-03-07 19:43:05 +0530

    Removed lectureProgressSchema as redundant

--- backend/models/courseProgress.js
+++ backend/models/courseProgress.js
@@ -1,15 +1,39 @@
 import mongoose from "mongoose";
 
-const lectureProgressSchema = new mongoose.Schema({
-	lectureId: { type: mongoose.Schema.Types.ObjectId, ref: "Lecture", required: true },
-	viewed: { type: Boolean, default: false },
-});
-
 const courseProgressSchema = new mongoose.Schema({
-	userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-	courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
-	completed: { type: Boolean, default: false },
-	lectureProgress: [lectureProgressSchema],
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+	courseId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Course",
+		required: true,
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+	},
+	lectureProgress: {
+		type: [
+			{
+				_id: false,
+				lectureId: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "Lecture",
+					required: true,
+				},
+				viewed: {
+					type: Boolean,
+					default: false,
+				},
+			},
+		],
+		default: [],
+	},
 });
 
+// Indexing for faster query
+courseProgressSchema.index({ userId: 1, courseId: 1 }, { unique: true });
 export const CourseProgress = mongoose.model("CourseProgress", courseProgressSchema);
