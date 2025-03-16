77c7574fcebb4ed17b5491042003de49916c2fc8
Kaushal <92015842+kaushalg47@users.noreply.github.com>
2025-03-16 11:45:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ app.use(
   cors({
     origin: "https://skill-pact-final.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
-    credentials: true, // Allows cookies/sessions
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]// Allows cookies/sessions
   })
 );
 
