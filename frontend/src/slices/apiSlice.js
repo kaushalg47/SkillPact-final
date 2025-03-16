15192f6f0094f89784f5c5cbb5e3094e2d8ce679
Kaushal <92015842+kaushalg47@users.noreply.github.com>
2025-03-16 14:25:35 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: ""
+  baseUrl: "/api"
 });
 
 export const apiSlice = createApi({
